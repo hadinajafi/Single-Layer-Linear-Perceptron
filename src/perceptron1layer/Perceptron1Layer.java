09fbe90d3b09f6eaf9adbf7c47809fab01c539e2
Hadi Najafi <hadinajafi.77@gmail.com>
2020-05-17 19:24:26 +0430

    Update Perceptron1Layer.java

--- src/perceptron1layer/Perceptron1Layer.java
+++ src/perceptron1layer/Perceptron1Layer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package perceptron1layer;
 
 /**
@@ -20,11 +15,12 @@ public class Perceptron1Layer {
         double[] p2 = {-1, 1, -1, -1};
         double[] p3 = {1, 1, 1, -1};
         double[] p4 = {1, -1, -1, 1};
+        //creating samples
         Sample s1 = new Sample(p1, 1, depth);
         Sample s2 = new Sample(p2, 1, depth);
         Sample s3 = new Sample(p3, -1, depth);
         Sample s4 = new Sample(p4, -1, depth);
-
+        //running the algorithm
         Training tr = new Training(depth);
         tr.addSample(s1);
         tr.addSample(s2);
